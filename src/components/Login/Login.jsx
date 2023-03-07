f3e1a274f778a3d170bdd4add7cf0d356d390ec3
parkhyeonji <iamphj3@naver.com>
2023-03-07 12:04:00 +0900

    style : themeprovider 적용 테스트

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const LoginComponent = () => {
-  return <div>Login 컴포넌트</div>;
+  return <StLoginWrapper>Login 컴포넌트</StLoginWrapper>;
 };
 
 export default LoginComponent;
+
+const StLoginWrapper = styled.div`
+  color: ${({ theme }) => theme.colors.main};
+  ${({ theme }) => theme.fonts.Pretendard_Title1_ExtraBold_24};
+`;
