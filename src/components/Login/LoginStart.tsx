96091c9a27f3756a707de7fac1bd92f4a6dc7288
SO-HUII <haejuuun925@gmail.com>
2023-04-07 22:23:18 +0900

    style: LoginStart 전체 style 적용

--- src/components/Login/LoginStart.tsx
+++ src/components/Login/LoginStart.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import { NavLink, Route } from 'react-router-dom';
+import React, { CSSProperties } from 'react';
+import { Link, Route } from 'react-router-dom';
 // import styled from 'styled-components';
 import LoginGender from './LoginGender';
-// import logo from '/images/Howhair_Logo.png';
-// import illu from '/images/Login_Illustration.svg';
+import LogoImgLoad from '../common/LogoImg';
 
 const LoginStart = () => {
+  const divStyle: CSSProperties = {
+    textAlign: 'center',
+  };
+
+  const illuImg: CSSProperties = {
+    width: '350px',
+    height: '233px',
+  };
+
+  const pStyle: CSSProperties = {
+    textAlign: 'center',
+    color: '#363636',
+    fontSize: '16px',
+  };
+
+  const spanStyle: CSSProperties = {
+    color: '#7C00DE',
+    fontSize: '16px',
+  };
+
+  const buttonStyle: CSSProperties = {
+    width: '325px',
+    height: '52px',
+    backgroundColor: '#9F62ED',
+    borderRadius: '15px',
+    textAlign: 'center',
+    color: '#FFFFFF',
+  };
+
   return (
-    <>
-      <img src="/images/Howhair_Logo.png" alt="로고" />
-      <img src="/images/Login_Illustration.svg" alt="추천 일러" />
-      <p>
+    <div style={divStyle}>
+      <LogoImgLoad />
+      <img
+        src="/images/Login_Illustration.svg"
+        alt="추천 일러"
+        style={illuImg}
+      />
+      <p style={pStyle}>
         타입에 따라 스타일이나 미용실을 추천해드려요 <br />
-        My Type을 입력해보러 갈까요?
+        <span style={spanStyle}>My Type</span>을 입력해보러 갈까요?
       </p>
-      <button type="button">
-        <NavLink to="/LoginGender">My Type 입력하러가기</NavLink>
-      </button>
-    </>
+      <Link to="/LoginGender">
+        <button type="button" style={buttonStyle}>
+          My Type 입력하러가기
+        </button>
+      </Link>
+    </div>
   );
 };
-<Route path="LoginGender" element={<LoginGender />} />;
 export default LoginStart;
