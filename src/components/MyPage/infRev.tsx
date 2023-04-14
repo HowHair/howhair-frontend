daa82a9d3292ba79746c59e0000da43b26ad0285
SO-HUII <haejuuun925@gmail.com>
2023-04-15 00:26:51 +0900

    feat: Hearder 컴포넌트 추가

--- src/components/MyPage/infRev.tsx
+++ src/components/MyPage/infRev.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import { Header } from '../common/Header';
 
 const infRev = () => {
   return (
     <>
-      <h2>내 정보 수정</h2>
+      <Header title="내 정보 수정" />
       <hr />
       <h2>연결된 카카오 계정 ${}님</h2>
-      <hr />
       <h2>My Type</h2>
       <button>수정</button>
       {/* 수정 버튼 클릭시 수정할 수 있는 드롭다운 나타내기 */}
       {/* 로그인 시 선택한 정보 가져오기 */}
       <h2>성별</h2>
       {/* 성별은 수정 X */}
-      <p></p>
       <h2>머리 기장</h2>
       {/* 드롭다운 */}
       <h2>곱슬 정도</h2>
