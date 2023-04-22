15d394448e8006f5de8184a4b3fcfad2f2fa6032
SO-HUII <haejuuun925@gmail.com>
2023-04-22 22:16:34 +0900

    feat: 하단바 구현

--- src/components/common/bottomBar.tsx
+++ src/components/common/bottomBar.tsx
@@ -1,28 +1,46 @@
 import React, { CSSProperties } from 'react';
-
-const navStyle: CSSProperties = {
-  overflow: 'hidden',
-};
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faScissors } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+import theme from '../../styles/theme';
 
 const buttonStyle: CSSProperties = {
   /* 수평정렬, 3개의 button을 각각 30% width만큼 할당 */
   textAlign: 'center',
   float: 'left',
-  width: '30%',
+  width: '33%',
 
   /* 세로길이 설정 */
-  height: '45px',
-  lineHeight: '45px',
+  height: '91px',
+  lineHeight: '91px',
 };
 
-const bottomBar = () => {
+const BottomBar = () => {
   return (
-    <nav className="wrapper" style={navStyle}>
-      <div style={buttonStyle}>button1</div>
-      <div style={buttonStyle}>button2</div>
-      <div style={buttonStyle}>button3</div>
-    </nav>
+    <Nav>
+      <div style={buttonStyle}>
+        <FontAwesomeIcon icon={faHouse} />
+      </div>
+      <div style={buttonStyle}>
+        <FontAwesomeIcon icon={faScissors} />
+      </div>
+      <div style={buttonStyle}>
+        <FontAwesomeIcon icon={faCircleUser} />
+      </div>
+    </Nav>
   );
 };
 
-export default bottomBar;
+export default BottomBar;
+
+const Nav = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 45px;
+  overflow: hidden;
+  border-top: 1px solid ${theme.colors.un_main};
+`;
