f04f250f76cf33230577749111093d83827272b8
SO-HUII <haejuuun925@gmail.com>
2023-05-04 01:23:58 +0900

    style: 헤더 변경 및 소제목 style 추가

--- src/components/Review/review.tsx
+++ src/components/Review/review.tsx
@@ -5,43 +5,48 @@ import MyDatePicker from '../common/DropDown/Calendar';
 import { ButtomButton } from '../common/Button/ButtomButton';
 import StarRating from '../common/Button/StarPoint';
 import MyBackButton from '../common/back';
+import { Header } from '../common/Header';
+import styled from 'styled-components';
+import theme from '../../styles/theme';
 
 const reviewPage = () => {
   return (
     <>
       {/* 뒤로가기 */}
-      <MyBackButton />
-      <h2>리뷰작성</h2>
+      <Header title="리뷰 작성" />
       {/* 검색 선택 지도 */}
       <input type="text" placeholder="미용실을 검색해보세요." />
-      <h2>시술 만족도</h2>
+      <Menu>시술 만족도</Menu>
       {/* 별점 */}
       <StarRating />
       <Line />
-      <h2>시술 날짜</h2>
+      <Menu>시술 날짜</Menu>
       {/* 날짜 드롭다운 + 달력 */}
       <MyDatePicker />
       <Line />
-      <h2>디자이너</h2>
+      <Menu>디자이너</Menu>
       <input type="text" placeholder="디자이너 이름을 입력해주세요." />
       <Line />
-      <h2>시술 종류</h2>
-      <p>(중복 선택 가능)</p>
-      {/* 드롭다운 시술 종류 */}
+      <Flex>
+        <Menu>시술 종류</Menu>
+        <Repet>(중복 선택 가능)</Repet>
+        {/* 드롭다운 시술 종류 */}
+      </Flex>
       <HairTypeDropdown />
-      {/* 시술 클릭시 클릭한 시술 종류 리스트로 생기게 */}
       <Line />
-      <h2>헤어 스타일</h2>
-      <p>(중복 선택 가능)</p>
-      {/* 드롭다운 헤어 스타일 종류 */}
+      <Flex>
+        <Menu>헤어 스타일</Menu>
+        <Repet>(중복 선택 가능)</Repet>
+        {/* 드롭다운 헤어 스타일 종류 */}
+      </Flex>
       <Line />
-      <h2>머리 기장</h2>
+      <Menu>머리 기장</Menu>
       {/* 드롭다운 기장 종류 */}
       <Line />
-      <h2>시술 가격</h2>
+      <Menu>시술 가격</Menu>
       <input type="text" placeholder="가격 입력" />
       <Line />
-      <h2>추가 내용</h2>
+      <Menu>추가 내용</Menu>
       {/* 사진 추가 */}
       {/* 내용 추가 */}
       <textarea
@@ -57,3 +62,23 @@ const reviewPage = () => {
 };
 
 export default reviewPage;
+
+const Menu = styled.div`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  ${theme.fonts.Pretendard_Subtitle2_Semibold_18}
+  ${theme.colors.head_color}
+`;
+
+const Repet = styled.div`
+  padding-top: 4px;
+  margin-left: 3px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  ${theme.fonts.Pretendard_caption3_regular_12}
+  ${theme.colors.gr_2}
+`;
+
+const Flex = styled.div`
+  display: flex;
+`;
