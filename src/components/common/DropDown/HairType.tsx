297304be32acd59d0afbf0f64fc49b8e1e4ee1ee
SO-HUII <haejuuun925@gmail.com>
2023-05-06 00:49:01 +0900

    style: 시술 종류 드롭다운 style 추가

--- src/components/common/DropDown/HairType.tsx
+++ src/components/common/DropDown/HairType.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
 
 type HairType = 'SELECT' | 'CUT' | 'PERM' | 'COLOR' | 'MAGIC';
 
@@ -10,14 +12,24 @@ function HairTypeDropdown() {
   }
 
   return (
-    <select value={hairType} onChange={handleSelect}>
+    <Drop value={hairType} onChange={handleSelect}>
       <option value="SELECT">시술 종류 선택</option>
       <option value="CUT">커트</option>
       <option value="PERM">펌</option>
       <option value="COLOR">염색</option>
       <option value="MAGIC">매직</option>
-    </select>
+    </Drop>
   );
 }
 
 export default HairTypeDropdown;
+
+const Drop = styled.select`
+  width: auto;
+  height: 35px;
+  background-color: ${theme.colors.fourth};
+  color: ${theme.colors.gr_2};
+  border-radius: 10px;
+  padding: 9px 15px 10px 15px;
+  border: none;
+`;
